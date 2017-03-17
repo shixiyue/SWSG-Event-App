febd86eb5a87947ec4b27ea4a397863c52d5a3fd
SorataYuu <jeremyjds@me.com>
2017-03-17 13:19:30 +0800

    Related Mentor System

--- SWSG/System.swift
+++ SWSG/System.swift
@@ -21,4 +21,22 @@ struct System {
         return participant
     }
     
+    public static var mentors: [Mentor] {
+        var mentors = [Mentor]()
+        let image = UIImage(named: "Profile")
+        let profile = Profile(name: "Mr Tan Hwee Huat", image: image!, job: "Asset Manager",
+                              company: "UOB Pte. Ltd.", country: "Singapore",
+                              education: "National University of Singapore",
+                              skills: "Financial Planning", description: "Awesome guy")
+        
+        for _ in 0...4 {
+            let mentor = Mentor(profile: profile, field: .business)
+            mentor.addSlots(on: Date.date(from: "2017-07-21"))
+            mentor.addSlots(on: Date.date(from: "2017-07-22"))
+            mentor.addSlots(on: Date.date(from: "2017-07-23"))
+            mentors.append(mentor)
+        }
+        
+        return mentors
+    }
 }
