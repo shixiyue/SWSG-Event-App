c6a25ad009452f565a88089e2ae4a14d9e150107
LiXiaoooowei <lixiaowei0535@gmail.com>
2017-04-10 09:47:05 +0800

    add secret file

---
+++ SWSG/Secret.swift
@@ -0,0 +1,17 @@
+//
+//  Secret.swift
+//  SWSG
+//
+//  Created by dinhvt on 10/4/17.
+//  Copyright © 2017 nus.cs3217.swsg. All rights reserved.
+//
+
+import Foundation
+
+class Secret {
+    
+    public static let oneSignalAppId = "7b1c79e0-7eb7-4cea-85a0-5aa12bea9e3a"
+    public static let oneSignalApiKey = "ZmY2OGU2ZTMtOTQyYS00YzNhLWI3M2EtMjY2MTU1NDEyYjNh"
+    public static let oneSignalAuthHeaderValue = "Basic " + oneSignalApiKey
+    
+}
